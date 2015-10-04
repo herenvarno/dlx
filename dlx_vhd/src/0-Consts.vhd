404dcc36a5db7fe989071c18a956088656cdccd7
herenvarno <herenvarno@live.com>
2015-10-04 15:48:22 +0200

    fix bug

--- dlx_vhd/src/0-Consts.vhd
+++ dlx_vhd/src/0-Consts.vhd
@@ -31,7 +31,7 @@ package Consts is
 	constant C_REG_WINDOW_NUM	: integer	:= 8;			-- Number of Windows in register file
 	constant C_RAM_IRAM_SIZE	: integer	:= 10240;		-- IRAM size
 	constant C_RAM_DRAM_SIZE	: integer	:= 1024;		-- DRAM size
-	constant C_BPU_ADDR_SIZE	: integer	:= 2;			-- BPU ADDR SIZE
+	constant C_BPU_ADDR_SIZE	: integer	:= 5;			-- BPU ADDR SIZE
 	
 	-- ALU Operations
 	constant OP_ADD		: std_logic_vector(C_CTR_CALU_SIZE-1 downto 0) := "00000";	--0x00
